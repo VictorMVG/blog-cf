4441d98b4b975fd5f749775d2e72c5ee79c8816d
VictorMVG <victorm.vlg@gmail.com>
2023-11-30 12:31:07 -0600

    Creation of controllers, routes and views of the course module

---
+++ resources/views/cursos/show.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Course:') }} {{ $curso }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 dark:bg-gray-800">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                <p>Bienvenido a la página del curso: {{ $curso }}</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
 
 Route::middleware([
     'auth:sanctum',
@@ -25,4 +25,10 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::controller(CursoController::class)->group(function () {
+        Route::get('cursos', 'index')->name('cursos.index');
+        Route::get('cursos/create', 'create')->name('cursos.create');
+        Route::get('cursos/{curso}', 'show')->name('cursos.show');
+    });
 });
